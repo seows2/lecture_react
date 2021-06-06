b7dcecc93fda299c28c713868bac045fe8f4e74f
seows2 <seows2@naver.com>
2021-06-07 01:52:22 +0900

    Search Form View

---
+++ 1-vanilla/js/views/SearchFormView.js
@@ -0,0 +1,14 @@
+import { qs } from "../helpers.js";
+import View from "./View.js";
+
+export default class SearchFormView extends View{
+    constructor(){
+        super(qs("#search-form-view"));
+        this.resetElement = qs("[type=reset]", this.element);
+        this.showResetButton(false);
+    }
+
+    showResetButton(visible = true){
+        this.resetElement.style.display = visible ? "block" : "none"
+    }
+}
