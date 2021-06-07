1183c5a3335a41c68fdf622b9c175b2aaf4348b7
seows2 <seows2@naver.com>
2021-06-07 16:39:28 +0900

    reset 이벤트 발행

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -10,10 +10,16 @@ export default class Controller {
   }
 
   subscribeViewEvents() {
-    this.searchFormView.on("@submit", event => this.search(event.detail.value));
+    this.searchFormView
+      .on("@submit", event => this.search(event.detail.value))
+      .on("@reset", () => this.reset());
   }
 
   search(keyword) {
     console.log(keyword);
+  }
+
+  reset() {
+    console.log("reset");
   }
 }
