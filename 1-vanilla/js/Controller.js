59edc9064cd56cb8c9a2f12525e97adf8f676568
seows2 <seows2@naver.com>
2021-06-07 17:35:53 +0900

    검색결과 기능 추가

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,10 +1,11 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, {searchFormView}) {
+  constructor(store, { searchFormView, searchResultView }) {
     this.store = store;
 
     this.searchFormView = searchFormView;
+    this.searchResultView = searchResultView;
 
     this.subscribeViewEvents();
   }
@@ -15,11 +16,23 @@ export default class Controller {
       .on("@reset", () => this.reset());
   }
 
-  search(keyword) {
-    console.log(keyword);
+  search(searchKeyword) {
+    this.store.search(searchKeyword);
+    this.render();
   }
 
   reset() {
-    console.log("reset");
+    this.store.searchKeyword = "";
+    this.store.searchResult = [];
+    this.render();
+  }
+
+  render() {
+    if(this.store.searchResult.length > 0) {
+      this.searchResultView.show(this.store.searchResult);
+      return;
+    }
+
+    this.searchResultView.hide();
   }
 }
--- 1-vanilla/js/main.js
+++ 1-vanilla/js/main.js
@@ -2,6 +2,7 @@ import Controller from "./Controller.js";
 import Store from "./store.js";
 import storage from "./storage.js";
 import SearchFormView from "./views/SearchFormView.js"
+import SearchResultView from "./views/SearchResultView.js";
 
 document.addEventListener("DOMContentLoaded", main);
 
@@ -9,7 +10,8 @@ function main() {
   const store = new Store(storage);
 
   const views = {
-    searchFormView : new SearchFormView()
+    searchFormView : new SearchFormView(),
+    searchResultView : new SearchResultView(),
   };
 
   new Controller(store, views);
