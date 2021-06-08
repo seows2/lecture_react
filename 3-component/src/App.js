8274b8926b6b6a899d12a280f9231c22a487da51
seows2 <seows2@naver.com>
2021-06-09 01:35:25 +0900

    리액트 검색 폼

---
+++ 3-component/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
+
+export default class App extends React.Component {
+    search(searchKeyword) {
+        console.log(searchKeyword);
+    }
+
+    handleReset() {
+        console.log("handleReset");
+    }
+
+    render() {
+        return (
+        <>
+            <Header title="검색"/>
+            <div className="container">
+                <SearchForm
+                    onSubmit={(searchKeyword) => this.search(searchKeyword)}
+                    onReset={() => this.handleReset()}
+                />
+            </div>
+        </>
+        )
+    }
+}
